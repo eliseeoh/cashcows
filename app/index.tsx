09dfaf0772c5b2261599abbb222cb6d3ef1a4552
Qiu YuTong <qiuyutong@Qius-MacBook-Air.local>
2024-06-28 06:22:41 +0800

    change to firebase 3

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,74 @@
 import React from 'react';
 import { PaperProvider } from 'react-native-paper';
-import {LoginScreen} from './login/login.screen';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider, AuthContext } from '../authentication/authContext';
+import { LoginScreen } from './login/login.screen';
 import { RegisterScn } from './login/register.screen';
-import {Homepage} from './main/homepage.screen';
+import { Homepage } from './main/homepage.screen';
+import { Edit } from './main/editprof.screen';
 
 const Stack = createNativeStackNavigator();
+
 function AuthStack() {
-	return (
-		<Stack.Navigator initialRouteName="Log in">
-				<Stack.Screen
-					name="Log in"
-					component={LoginScreen}
-          options={{ headerShown: false }}/>
-				<Stack.Screen
-					name="Register"
-					component={RegisterScn}
-          options={{ 
-            title: ""}}/>
-		</Stack.Navigator>
-	);
+  return (
+    <Stack.Navigator initialRouteName="Log in">
+      <Stack.Screen
+        name="Log in"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScn}
+        options={{ title: "" }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 function AppStack() {
-	return (
-		<Stack.Navigator>
-			<Stack.Screen name="home" component={Homepage}/>
-		</Stack.Navigator>
-	)
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Homepage}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Edit Profile"
+        component={Edit}
+      />
+    </Stack.Navigator>
+  );
 }
+
 export default function Index() {
   return (
-	<PaperProvider>
-		<AuthStack />
-	</PaperProvider>
+    <PaperProvider>
+      <AuthProvider>
+        <AuthContext.Consumer>
+          {({ authData }) => (
+            <NavigationContainer>
+              <Stack.Navigator>
+                {authData == null ? (
+                  <Stack.Screen
+                    name="Auth"
+                    component={AuthStack}
+                    options={{ headerShown: false }}
+                  />
+                ) : (
+                  <Stack.Screen
+                    name="App"
+                    component={AppStack}
+                    options={{ headerShown: false }}
+                  />
+                )}
+              </Stack.Navigator>
+            </NavigationContainer>
+          )}
+        </AuthContext.Consumer>
+      </AuthProvider>
+    </PaperProvider>
   );
 }
