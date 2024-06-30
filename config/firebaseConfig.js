9fb8eaaf68e61c24e690818320bdad8af6acbb45
fishiebloop <67816767+fishiebloop@users.noreply.github.com>
2024-06-30 22:53:27 +0800

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,19 +3,27 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCxgpWsVXck0X_jXZjCjIaLV_uO0qVZaJU",
-    authDomain: "cashcows-4102f.firebaseapp.com",
-    projectId: "cashcows-4102f",
-    storageBucket: "cashcows-4102f.appspot.com",
-    messagingSenderId: "3300826133",
-    appId: "1:3300826133:android:7cbd57d9d1e2f3349b821c",
-    measurementId: "G-4SHD1K0CC5",
+  apiKey: "AIzaSyCxgpWsVXck0X_jXZjCjIaLV_uO0qVZaJU",
+  authDomain: "cashcows-4102f.firebaseapp.com",
+  projectId: "cashcows-4102f",
+  storageBucket: "cashcows-4102f.appspot.com",
+  messagingSenderId: "3300826133",
+  appId: "1:3300826133:android:7cbd57d9d1e2f3349b821c",
+  measurementId: "G-4SHD1K0CC5",
 };
-   
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication with persistence
 export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
-export const firestore = getFirestore(app);
+
+// Initialize Firestore
+export const db = getFirestore(app);
+
+// Export the app for further use
+export { app };
