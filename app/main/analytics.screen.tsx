a0a196ddd1ed3179fe96c536f09b776a10334ba6
Elise Oh <elise.ohh1506@gmail.com>
2024-07-28 14:31:10 +0800

    add tab bar icons

---
+++ app/main/analytics.screen.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { View, Alert, Text} from "react-native";
+
+export const Analytics = () => {
+    return (
+        <Text>analytics</Text>
+    )
+}
